e1f437d05990e93a4bc1d7d64cfe22f2d9127504
Christian Reyes <cmreyesqla598@gmail.com>
2019-12-16 15:52:45 -0800

    menu

---
+++ controllers/MenuController.js
@@ -0,0 +1,36 @@
+let inquirer = require("inquirer");
+
+module.exports = class MenuController {
+  constructor() {
+    this.mainMenuQuestions = [
+      {
+        type: "list",
+        name: "mainMenuChoice",
+        message: "Please choose from an option below: ",
+        choices: ["Add new contact", "Exit"]
+      }
+    ];
+    this.contacts = [];
+  }
+  main() {
+    console.log("Welcome to AddressBuddy!");
+    inquirer.prompt(this.mainMenuQuestions).then((response) => {
+        switch(response.mainMenuChoice){
+            
+        }
+    })
+  }
+  clear() {
+    console.log("\x1Bc");
+  }
+  addContact() {
+    this.clear();
+    console.log("addContact called");
+    this.main();
+  }
+
+  exit() {
+    console.log("AddressBuddy says goodbye!");
+    process.exit();
+  }
+};
