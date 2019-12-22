44e07a589c2e767cca87eee07d9a81f7bbc59232
Christian <chreyes700qla@gmail.com>
2019-12-22 13:09:32 -0800

    edits

--- controllers/ContactController.js
+++ controllers/ContactController.js
@@ -1,6 +1,11 @@
 let inquirer = require("inquirer");
+const Contact = require("../db/models").Contact;
+
 module.exports = class ContactController {
     constructor(){
-
+this.contacts = []
+    }
+    addContact(name, phone){
+        return Contact.create({name, phone})
     }
 }
--- spec/menu_controller_spec.js
+++ spec/menu_controller_spec.js
@@ -3,13 +3,5 @@ describe("MenuController", () => {
   beforeEach(() => {
     this.menu = new MenuController()
   })
-  describe("#getContactCount()", () => {
-    it("should return 0 when no contacts are in the book", () => {
-      expect(this.menu.getContactCount()).toBe(0)
-    })
-    it("should return 1 when there is only one contact in the book", () => {
-      this.menu.contacts.push("Jerry");
-      expect(this.menu.getContactCount()).toBe(1)
-    })
-  })
+  
 })
